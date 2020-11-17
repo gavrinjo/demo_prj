3126ddd48f46ddd7aac723567720e7acf10cf029
gavrinjo <drazen.gavric@gmail.com>
2020-11-17 15:30:38 +0100

    copy by excel list revised

--- _copy_by_xl.py
+++ _copy_by_xl.py
@@ -11,7 +11,7 @@ log_file = "error_logfile"
 log = log.get_logger(log_file)
 
 time_now = datetime.now().strftime("%Y-%m-%d_%H%M%S")       # date/time in format as (Y-m-d_HMS)
-xl_file = Path("D:\\00_HERNE\\test\\test_copy.xlsx")     # xlsx file
+# xl_file = Path("D:\\00_HERNE\\test\\test_copy.xlsx")     # xlsx file
 
 
 def copy_xl(src, col1, col2, col3):
@@ -37,17 +37,14 @@ def copy_xl(src, col1, col2, col3):
     for row in range(1, ws.max_row + 1):
         try:
             if ws.cell(row, src_column).value is not None:
-                if ws.cell(row, dst_column).value is not None:
-                    if not Path(ws.cell(row, dst_column).value).parent.exists():
-                        Path(ws.cell(row, dst_column).value).parent.mkdir(parents=True, exist_ok=True)
-                    sh.copy2(ws.cell(row, src_column).value, ws.cell(row, dst_column).value)
-                else:
-                    raise Exception
+                if not Path(ws.cell(row, dst_column).value).parent.exists():
+                    Path(ws.cell(row, dst_column).value).parent.mkdir(parents=True, exist_ok=True)
+                sh.copy2(ws.cell(row, src_column).value, ws.cell(row, dst_column).value)
             else:
                 pass
         except Exception as error:
             log.exception(f"error copy {get_column_letter(src_column)}{row} --> {get_column_letter(dst_column)}{row} {error}", exc_info=True)
-            ws.cell(row, log_column, f"error copy {get_column_letter(src_column)}{row} --> {get_column_letter(dst_column)}{row}")
+            ws.cell(row, log_column, f"error copy {get_column_letter(src_column)}{row} --> {get_column_letter(dst_column)}{row} {error}")
         else:
             ws.cell(row, log_column, "successful copy")
 
