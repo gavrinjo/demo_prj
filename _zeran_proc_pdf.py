386910814a843a48964722a8302b2436bbd0f11b
gavrinjo <drazen.gavric@gmail.com>
2021-05-21 14:53:53 +0200

    some new things

--- _zeran_proc_pdf.py
+++ _zeran_proc_pdf.py
@@ -1,5 +1,6 @@
 from dir_list_r01 import dir_list
 from pathlib import Path
+from pdf_parser import parse, re_split, parse2, get_pdf_content_lines, parse3, parse4, parse5
 import os
 
 main_dir = Path("D:/_test_ground/_zeran")
@@ -15,4 +16,28 @@ test = all(map(lambda x, y: x == y, ref_ls, con_ls))
 
 
 print(any(x in ref_ls for x in ref_ls))
+
+file = Path(r"D:\_test_ground\_zeran\01_LBA_01_LB-HP\2018-07-25\Z214LBA25BR010_00.pdf")
+# file = Path(r"D:\_test_ground\_zeran\01_LBA_01_LB-HP\2018-07-25\Z214LBA10BR010_00.pdf")
+delimiters = " ", "\n"
+
+# print(parse(file))
+
+# print(list(filter(None, re_split(delimiters, parse(file),  maxsplit=0))))
+# print(re_split(delimiters, parse(file),  maxsplit=0))
+# print(parse2(file))
+
+# print(searchInPDF(file))
+
+"""from pdfminer.high_level import extract_pages
+from pdfminer.layout import LTTextLineHorizontal
+
+for page_layout in extract_pages(file):
+    for element in page_layout:
+        if isinstance(element, LTTextLineHorizontal):
+            print(element.get_text())"""
+
+
+# print(parse3(file))
+parse5(file)
 
--- pdf_parser.py
+++ pdf_parser.py
@@ -1,10 +1,13 @@
+import PyPDF2
+import re
 from io import StringIO
-from pdfminer.converter import TextConverter
-from pdfminer.layout import LAParams
+from pdfminer.converter import TextConverter, PDFPageAggregator
+from pdfminer.layout import LAParams, LTTextLineHorizontal, LTTextBox
 from pdfminer.pdfdocument import PDFDocument
-from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter, resolve1
 from pdfminer.pdfpage import PDFPage
 from pdfminer.pdfparser import PDFParser
+from pdfminer.high_level import extract_pages
 
 
 def parse(file):
@@ -22,4 +25,78 @@ def parse(file):
         for page in PDFPage.create_pages(doc):
             interpreter.process_page(page)
 
-    return output_string.getvalue()
+    pages = str(resolve1(doc.catalog["Pages"])["Count"])
+    content = output_string.getvalue()
+    return f"{pages} {content}"
+
+
+def re_split(delimiter, string, maxsplit=0):
+    rePattern = "|".join(map(re.escape, delimiter))
+    return re.split(rePattern, string, maxsplit)
+
+
+def parse2(file):
+    pdfContent = []
+    with open(file, "rb") as pdf_file:
+        pdfReader = PyPDF2.PdfFileReader(pdf_file)
+        for page in range(pdfReader.numPages):
+            pdfContent.append(pdfReader.getPage(page).extractText())
+
+    return pdfContent
+
+
+def get_pdf_content_lines(pdf_file_path):
+    with open(pdf_file_path, "rb") as f:
+        pdf_reader = PyPDF2.PdfFileReader(f)
+        for page in pdf_reader.pages:
+            for text in page.extractText().splitlines():
+                yield text
+
+
+def parse3(file):
+
+    output_string = StringIO()
+    with open(file, 'rb') as in_file:
+        parser = PDFParser(in_file)
+        doc = PDFDocument(parser)
+        rsrcmgr = PDFResourceManager()
+        device = PDFPageAggregator(rsrcmgr, laparams=LAParams(detect_vertical=True))
+        interpreter = PDFPageInterpreter(rsrcmgr, device)
+        layout = []
+        for page in PDFPage.create_pages(doc):
+            interpreter.process_page(page)
+            layout.append(device.get_result())
+
+        return layout
+
+
+def parse4(file):
+    with open(file, "rb") as pdfFileObj:
+        pdfRead = PyPDF2.PdfFileReader(pdfFileObj)
+        pageObj = pdfRead.getPage(1)
+        pages_text = pageObj.extractText()
+        for line in StringIO(pages_text):
+            print(line)
+
+
+def parse5(file):
+
+    #output_string = StringIO()
+    with open(file, 'rb') as in_file:
+        parser = PDFParser(in_file)
+        doc = PDFDocument(parser)
+        rsrcmgr = PDFResourceManager()
+        device = PDFPageAggregator(rsrcmgr, laparams=LAParams(detect_vertical=True, line_margin=2))
+        interpreter = PDFPageInterpreter(rsrcmgr, device)
+        for page in PDFPage.create_pages(doc):
+            interpreter.process_page(page)
+            layout = device.get_result()
+            for x in layout:
+                if isinstance(x, LTTextLineHorizontal):
+                    print(x.get_text().strip())
+
+    #pages = str(resolve1(doc.catalog["Pages"])["Count"])
+    #content = output_string.getvalue()
+    #return f"{pages} {content}"
+
+
